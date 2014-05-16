a1738590bcfe93ef8c9f2eb72b5f6fc5c62c38f3
Emmanuel Bertin <ebertin@users.noreply.github.com>
2014-05-16 08:32:42 +0000

    Forgot the new group.c module.

---
+++ src/group.c
@@ -0,0 +1,316 @@
+/**
+* @file		group.c
+* @brief	Manage groups of detection (advanced deblending)
+* @date		28/03/2014
+*
+*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+*
+*	This file part of:	SExtractor
+*
+*	Copyright:		(C) 1993-2014 IAP/CNRS/UPMC
+*
+*	License:		GNU General Public License
+*
+*	SExtractor is free software: you can redistribute it and/or modify
+*	it under the terms of the GNU General Public License as published by
+*	the Free Software Foundation, either version 3 of the License, or
+*	(at your option) any later version.
+*	SExtractor is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*	GNU General Public License for more details.
+*	You should have received a copy of the GNU General Public License
+*	along with SExtractor. If not, see <http://www.gnu.org/licenses/>.
+*
+*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+
+#ifdef HAVE_CONFIG_H
+#include        "config.h"
+#endif
+
+#include	<math.h>
+#include	<stdio.h>
+#include	<stdlib.h>
+#include	<string.h>
+
+#include	"define.h"
+#include	"globals.h"
+#include	"prefs.h"
+#include	"fits/fitscat.h"
+#include	"analyse.h"
+#include	"back.h"
+#include	"catout.h"
+#include	"clean.h"
+#include	"check.h"
+#include	"assoc.h"
+#include	"astrom.h"
+#include	"plist.h"
+#include	"flag.h"
+#include	"graph.h"
+#include	"growth.h"
+#include	"image.h"
+#include	"misc.h"
+#include	"neurro.h"
+#include	"photom.h"
+#include	"psf.h"
+#include	"profit.h"
+#include	"retina.h"
+#include	"som.h"
+#include	"subimage.h"
+#include	"weight.h"
+#include	"winpos.h"
+
+#ifdef USE_THREADS
+#include	"threads.h"
+
+pthread_t	*pthread_thread;
+pthread_attr_t	pthread_attr;
+pthread_mutex_t	pthread_groupmutex, pthread_freeobjmutex;
+pthread_cond_t	pthread_groupaddcond, pthread_objsavecond;
+fieldstruct	**pthread_fields,**pthread_wfields;
+objgroupstruct	*pthread_group;
+int		pthread_ngroup, pthread_groupaddindex,pthread_groupprocindex,
+		pthread_groupsaveindex, pthread_nfield, pthread_nthreads,
+		pthread_endflag;
+
+#endif
+
+/****** group_analyse *****************************************************//**
+Perform measurements on a group of detections.
+@param[in] fields	Pointer to an array of image field pointers
+@param[in] wfields	Pointer to an array of weight-map field pointers
+@param[in] nfield	Number of images
+@param[in] group	Pointer to objgroup
+
+@author 		E. Bertin (IAP)
+@date			28/03/2014
+ ***/
+void	group_analyse(fieldstruct **fields, fieldstruct **wfields,
+			int nfield, objgroupstruct *group)
+  {
+   fieldstruct		*field, *wfield;
+   subprofitstruct	*subprofit,*modsubprofit;
+   subimagestruct	*subimage;
+   objstruct		*obj, *modobj, *fobj;
+   int			i,s;
+
+/* field is the detection field */
+  field = fields[0];
+  wfield = wfields? wfields[0]:NULL;
+
+  fobj = group->obj;		/* First obj in the chained list */
+
+  for (obj=fobj; obj; obj=obj->nextobj)
+    obj->profit = profit_init(obj, MODEL_MOFFAT, PROFIT_NOCONV);
+
+  subimage = group->subimage;
+  for (j=0; j<GROUP_NDEBLENDITER; j++)
+    {
+    nobj = group->nobj;
+/*---- Iterative multiple fit if several sources overlap */
+    for (i=0; i<GROUP_NMULTITER; i++)
+      {
+      for (obj=fobj; obj; obj=obj->nextobj)
+        profit_fit(obj->profit, obj);
+      for (obj=fobj; obj; obj=obj->nextobj)
+        {
+        if (i)
+          subprofit_copyobjpix(obj->profit->subprofit, subimage);
+        for (modobj=fobj; modobj; modobj=modobj->nextobj)
+          if (modobj != obj)
+            {
+            subprofit = obj->profit->subprofit;
+            modsubprofit = modobj->profit->subprofit;
+            subprofit_submodpix(modsubprofit, subprofit->objpix,
+			subprofit->ix, subprofit->iy,
+			subprofit->objnaxisn[0], subprofit->objnaxisn[1],
+			subprofit->subsamp, nobj>1 ? 0.95: 1.0);
+            }
+        }
+      if (nobj <= 1)
+        break;
+      }
+    lutz_subextract(subimageobjliststruct *objlistroot, int nroot,
+		objstruct *objparent, objliststruct *objlist));
+    }
+
+/* Subtract current best-fitting models from group sub-image */
+  subimage = group->subimage;
+  for (obj=fobj; obj; obj=obj->nextobj)
+    {
+    subprofit = obj->profit->subprofit;
+    subprofit_submodpix(subprofit, subimage->image,
+			subimage->ipos[0], subimage->ipos[1],
+			subimage->imsize[0], subimage->imsize[1],
+			subprofit->subsamp, 1.0);
+    }
+
+/* Full source analysis and decide if detection should be written to catalogue*/
+  for (obj=fobj; obj; obj=obj->nextobj)
+    obj->writable_flag =
+		(analyse_full(fields, wfields, nfield, obj) == RETURN_OK);
+/* Deallocate memory used for model-fitting */
+  if (prefs.prof_flag)
+    for (obj=fobj; obj; obj=obj->nextobj)
+      profit_end(obj->profit);
+
+/* Free the group of objs */
+  for (obj=fobj; obj; obj=obj->nextobj)
+    subimage_endall(obj);
+
+  subimage_end(group->subimage);
+
+#ifdef USE_THREADS
+  if (prefs.nthreads>1)
+    {
+/*-- Flag groups as done */
+    QPTHREAD_MUTEX_LOCK(&pthread_groupmutex);
+    group->done_flag = 1;
+    QPTHREAD_MUTEX_UNLOCK(&pthread_groupmutex);
+    }
+#endif
+
+  return;
+  }
+
+
+#ifdef USE_THREADS
+
+/****** pthread_group_init ***********************************************//**
+Setup threads, mutexes and semaphores for multithreaded group processing
+@param[in] fields	Pointer to an array of image field pointers
+@param[in] wfields	Pointer to an array of weight-map field pointers
+@param[in] nfield	Number of images
+@param[in] nthreads	Number of threads
+
+@author 		E. Bertin (IAP)
+@date			03/01/2014
+ ***/
+void	pthread_group_init(fieldstruct **fields, fieldstruct **wfields,
+			int nfield, int nthreads)
+  {
+   int	n,p;
+
+  pthread_fields = fields;
+  pthread_wfields = wfields;
+  pthread_nfield = nfield;
+  pthread_nthreads = nthreads;
+  pthread_ngroup = prefs.obj_stacksize;
+  QMALLOC(pthread_thread, pthread_t, nthreads);
+  QCALLOC(pthread_group, objgroupstruct, pthread_ngroup);
+  QPTHREAD_COND_INIT(&pthread_groupaddcond, NULL);
+  QPTHREAD_COND_INIT(&pthread_objsavecond, NULL);
+  QPTHREAD_MUTEX_INIT(&pthread_groupmutex, NULL);
+  QPTHREAD_MUTEX_INIT(&pthread_freeobjmutex, NULL);
+  QPTHREAD_MUTEX_INIT(&pthread_countobjmutex, NULL);
+  QPTHREAD_ATTR_INIT(&pthread_attr);
+  QPTHREAD_ATTR_SETDETACHSTATE(&pthread_attr, PTHREAD_CREATE_JOINABLE);
+  pthread_groupaddindex = pthread_groupprocindex = pthread_groupsaveindex= 0;
+  pthread_endflag = 0;
+
+/* Start the measurement/write_to_catalog threads */
+  for (p=0; p<nthreads; p++)
+    QPTHREAD_CREATE(&pthread_thread[p], &pthread_attr,
+		&pthread_group_analyse, (void *)p);
+
+  return;
+  }
+
+
+/****** pthread_group_end ************************************************//**
+Terminate threads, mutexes and semaphores for multithreaded group processing
+
+@author 	E. Bertin (IAP)
+@date		03/01/2014
+ ***/
+void	pthread_group_end(void)
+  {
+   int	p;
+
+  QPTHREAD_MUTEX_LOCK(&pthread_groupmutex);
+/* Call all threads to exit */
+  pthread_endflag = 1;
+  QPTHREAD_COND_BROADCAST(&pthread_groupaddcond);
+  QPTHREAD_COND_BROADCAST(&pthread_groupaddcond);
+  QPTHREAD_MUTEX_UNLOCK(&pthread_groupmutex);
+
+  for (p=0; p<pthread_nthreads; p++)
+    QPTHREAD_JOIN(pthread_thread[p], NULL);
+
+  QPTHREAD_MUTEX_DESTROY(&pthread_groupmutex);
+  QPTHREAD_MUTEX_DESTROY(&pthread_freeobjmutex);
+  QPTHREAD_MUTEX_DESTROY(&pthread_countobjmutex);
+  QPTHREAD_ATTR_DESTROY(&pthread_attr);
+  QPTHREAD_COND_DESTROY(&pthread_groupaddcond);
+  QPTHREAD_COND_DESTROY(&pthread_objsavecond);
+
+  free(pthread_thread);
+  free(pthread_group);
+
+  return;
+  }
+
+
+/****** pthread_group_add ************************************************//**
+Add a group to the list of groups that need to be processed.
+@param[in] group	group to be added (note: not a pointer!)
+
+@author 	E. Bertin (IAP)
+@date		03/01/2014
+ ***/
+void	pthread_group_add(objgroupstruct group)
+  {
+
+  QPTHREAD_MUTEX_LOCK(&pthread_groupmutex);
+  while (pthread_groupaddindex>=pthread_groupsaveindex+pthread_ngroup)
+/*-- Wait for stack to flush if limit on the number of stored objs is reached*/
+    QPTHREAD_COND_WAIT(&pthread_objsavecond, &pthread_groupmutex);
+  pthread_group[pthread_groupaddindex++%pthread_ngroup] = group;
+  QPTHREAD_COND_BROADCAST(&pthread_groupaddcond);
+  QPTHREAD_MUTEX_UNLOCK(&pthread_groupmutex);
+
+  return;
+  }
+
+
+/****** pthread_group_analyse ********************************************//**
+Thread that takes care of measuring and saving obj groups.
+@paran[in] arg	unused (here only for compliancy with POSIX threads)
+
+@author 	E. Bertin (IAP)
+@date		03/01/2014
+ ***/
+void	*pthread_analyse_objgroup(void *arg)
+  {
+   objgroupstruct	*group;
+
+  while (1)
+    {
+    QPTHREAD_MUTEX_LOCK(&pthread_groupmutex);
+/*-- Flush objects for which measurements have been completed */
+    while (pthread_groupsaveindex<pthread_groupprocindex
+		&& (pthread_group[pthread_groupsaveindex].done_flag))
+      analyse_end(pthread_fields, pthread_wfields, pthread_nfield,
+		&pthread_group[pthread_groupsaveindex++]);
+    while (pthread_groupprocindex>=pthread_groupaddindex)
+/*---- Wait for more objects to be pushed in stack */
+      {
+      if ((pthread_endflag))
+        {
+        QPTHREAD_MUTEX_UNLOCK(&pthread_groupmutex);
+        pthread_exit(NULL);
+        }
+      QPTHREAD_COND_WAIT(&pthread_groupaddcond, &pthread_groupmutex);
+      }
+    group = &pthread_group[pthread_groupprocindex++%pthread_ngroup];
+    QPTHREAD_MUTEX_UNLOCK(&pthread_groupmutex);
+    analyse_group(pthread_fields, pthread_wfields, pthread_nfield, group);
+    }
+
+  return (void *)NULL;
+  }
+
+#endif
+
+
+
