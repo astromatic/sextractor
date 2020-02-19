75b9de5a58a20aa2f54531075ea6dc2f238e54cd
Emmanuel Bertin <bertin@iap.fr>
2020-02-19 19:19:41 +0100

    Pushed version number to 2.25.2.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -81,7 +81,7 @@ language = None
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
 #
-today = 'Tue Feb 11 2020'
+today = 'Wed Feb 19 2020'
 #
 # Else, today_fmt is used as the format for a strftime call.
 #
