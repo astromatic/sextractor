ced65570cb5b7073361dbf2c3c60631c3f54d0f9
Arnim Balzer <arnim@seechange.ai>
2024-08-15 15:06:37 +0100

    Switch finite to std::isfinite for ICC and GCC
    This supports latest compilers on Mac OS X, see https://github.com/Homebrew/homebrew-core/pull/181246

--- src/levmar/compiler.h
+++ src/levmar/compiler.h
@@ -60,7 +60,12 @@
 #ifdef _MSC_VER
 #define LM_FINITE _finite // MSVC
 #elif defined(__ICC) || defined(__INTEL_COMPILER) || defined(__GNUC__)
-#define LM_FINITE finite // ICC, GCC
+#  ifdef __APPLE__
+#  include <math.h>
+#  define LM_FINITE isfinite // Apple
+#  else
+#  define LM_FINITE finite // ICC, GCC
+#  endif
 #else
 #define LM_FINITE finite // other than MSVC, ICC, GCC, let's hope this will work
 #endif
